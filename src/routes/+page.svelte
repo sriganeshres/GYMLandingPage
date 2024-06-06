626ebdd93b8edac63d2f769ff7d225c67c4d4106
sriganeshres <sriganeshthota12345@gmail.com>
2024-06-07 03:08:32 +0530

    Added all the files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Conversion from '../components/Conversion.svelte';
+	import Faq from '../components/Faq.svelte';
+	import Hero from '../components/Hero.svelte';
+	import Product from '../components/Product.svelte';
+	import Reviwes from '../components/Reviwes.svelte';
+</script>
+
+<main class="flex flex-col">
+	<Hero />
+	<Product />
+	<Reviwes />
+	<Faq />
+	<Conversion />
+</main>
